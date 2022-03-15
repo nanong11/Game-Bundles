19abadd2db6ce4e593a6796d1e0f9ec9b5c02d24
nanong11 <marcallen.nanong11@gmail.com>
2022-03-15 22:47:04 +0800

    Add verify admin when add a game

--- client/js/addGame.js
+++ client/js/addGame.js
@@ -1,5 +1,6 @@
 const addGameForm = document.querySelector(`.addGameForm`)
 const addGameRequest = `https://tranquil-caverns-53550.herokuapp.com/api/games/create`
+const profile = `https://tranquil-caverns-53550.herokuapp.com/api/users/profile`
 
 const token = localStorage.getItem(`token`)
 
@@ -11,23 +12,35 @@ addGameForm.addEventListener(`submit`, (e) => {
     const stock = document.querySelector(`#stock`).value
     
     if(token){
-        fetch(addGameRequest, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": token
-            },
-            body: JSON.stringify({
-                gameName, description, price, stock
-            })    
+        fetch(profile, {
+            method: "GET",
+            headers: {"Authorization": token}
+
         })
         .then(result => result.json())
         .then(result => {
-            if(result){
-                alert(`${result.gameName} successfully added`)
-                window.location.replace(`../admin/admin-view-games.html`)
+            if(result.isAdmin){
+                fetch(addGameRequest, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": token
+                    },
+                    body: JSON.stringify({
+                        gameName, description, price, stock
+                    })    
+                })
+                .then(result => result.json())
+                .then(result => {
+                    if(result){
+                        alert(`${result.gameName} successfully added`)
+                        window.location.replace(`../admin/admin-view-games.html`)
+                    }else{
+                        return window.location.href = `../../error.html`                
+                    }
+                })
             }else{
-                return window.location.href = `../../error.html`                
+                return window.location.href = `../../error.html`
             }
         })
     }else{
