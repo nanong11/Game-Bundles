cf331fe4cdbdcd53fbed078dbe8dc76cbc9a478e
nanong11 <marcallen.nanong11@gmail.com>
2022-03-15 17:22:54 +0800

    Add games counter in admin Dashboard

--- client/js/admin.js
+++ client/js/admin.js
@@ -16,6 +16,20 @@ window.addEventListener(`load`, () => {
                 return window.location.href = `../../error.html`
             }
         })
+        fetch(`https://tranquil-caverns-53550.herokuapp.com/api/games`, {
+            headers: {
+                "Authorization": token
+            }
+        })
+        .then(result => result.json())
+        .then(result => {
+            if(result){
+                const gamesCounter = document.querySelector(`#gamesCounter`)
+                gamesCounter.innerText = result.length
+            }else{
+                return window.location.href = `../../error.html`
+            }
+        })
     }else{
         return window.location.href = `../../error.html`
     }
